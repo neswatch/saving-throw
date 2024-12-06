962a69b9e7543da5ad9ce569914598670c0ae3d2
Enzo Calleya <enzouz12@gmail.com>
2024-12-06 02:17:52 +0100

    merge with main

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Count from "./pages/count/Count.tsx";
 import Minijeu from "./pages/count/Minijeu/Minijeu.tsx";
+import Temperature from "./pages/temperature/Temperature.tsx";
+import Poumon from "./pages/play/poumon.tsx";
 
 export default function Router() {
   return (
@@ -8,6 +10,8 @@ export default function Router() {
       <Routes>
         <Route path={"/"} element={<Count />} />
         <Route path={"/mj"} element={<Minijeu />} />
+        <Route path={"/temperature"} element={<Temperature />} />
+        <Route path={"/poumon"} element={<Poumon />} />
       </Routes>
     </BrowserRouter>
   );
