7f8d3731c867d784d651889aca63fe6d2e9dd664
Enzo Calleya <enzouz12@gmail.com>
2024-12-06 02:03:21 +0100

    feat: minigame page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Count from "./pages/count/Count.tsx";
+import Minijeu from "./pages/count/Minijeu/Minijeu.tsx";
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<Count/>} />
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Count />} />
+        <Route path={"/mj"} element={<Minijeu />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
